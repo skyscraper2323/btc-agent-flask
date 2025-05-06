d134abb1397c6f97c263d0f91df33f941381de4f
skyscraper2323 <skyscraper23@gmail.com>
2025-05-06 10:36:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import requests
 import datetime
+import os
 
 app = Flask(__name__)
 
@@ -23,3 +24,7 @@ def get_btc_price():
         return jsonify({"timestamp": now, "btc_usd": price})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
