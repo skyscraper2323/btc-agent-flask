baa0ed3705ad5602cecbfca7c03b9a227d67f9f6
skyscraper2323 <skyscraper23@gmail.com>
2025-05-06 09:39:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import requests
 import datetime
 
@@ -12,8 +12,8 @@ def home():
 def get_btc_price():
     try:
         now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice/BTC.json")
-        price = round(response.json()["bpi"]["USD"]["rate_float"], 2)
+        response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+        price = response.json()["bitcoin"]["usd"]
         return jsonify({"timestamp": now, "btc_usd": price})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
